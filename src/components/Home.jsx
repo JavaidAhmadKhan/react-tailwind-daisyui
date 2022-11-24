dcfb2e6543e5f96d5ce0fae2d3e1128da0ec1306
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-11-24 11:25:09 +0530

    initialsed with react tailwind daisy ui

---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import DataTable from './DataTable'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="carousel w-full h-[500px] ">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='slider' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='slider' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='slider' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='slider' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+            <DataTable />
+            <section className='pt-12 pb-12'>
+                <div className="card lg:card-side bg-base-100 shadow-xl">
+                    <figure><img src="https://placeimg.com/400/400/arch" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New album is released!</h2>
+                        <p>Click the button to listen on Spotiwhy app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home
