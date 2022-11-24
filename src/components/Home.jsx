df14ddcab4289e327eb7556a8472b828ecd41c6c
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-11-24 13:21:59 +0530

    removed product card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "./ProductCard"
+
 
 const products = [
     {
@@ -39,7 +39,6 @@ const products = [
 export default function Home() {
     return (
         <div className="bg-white">
-            <ProductCard />
             <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Products</h2>
 
