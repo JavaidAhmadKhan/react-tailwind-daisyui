8645a84f16f70b1d9e7a924a9faf654189da38af
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-11-24 13:58:03 +0530

    updated product images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,22 +9,22 @@ const Footer = () => {
                         <h5 className="text-xl font-bold mb-6">Features</h5>
                         <ul className="list-none footer-links">
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Cool stuff</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Cool stuff</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Random feature</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Random feature</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team feature</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team feature</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Stuff for developers</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Stuff for developers</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another one</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another one</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Last time</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Last time</a>
                             </li>
                         </ul>
                     </div>
@@ -32,16 +32,16 @@ const Footer = () => {
                         <h5 className="text-xl font-bold mb-6">Resources</h5>
                         <ul className="list-none footer-links">
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource name</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource name</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another resource</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another resource</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Final resource</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Final resource</a>
                             </li>
                         </ul>
                     </div>
@@ -49,16 +49,16 @@ const Footer = () => {
                         <h5 className="text-xl font-bold mb-6">About</h5>
                         <ul className="list-none footer-links">
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Locations</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Locations</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Privacy</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Privacy</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Terms</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Terms</a>
                             </li>
                         </ul>
                     </div>
@@ -66,26 +66,26 @@ const Footer = () => {
                         <h5 className="text-xl font-bold mb-6">Help</h5>
                         <ul className="list-none footer-links">
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Support</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Support</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Help Center</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Help Center</a>
                             </li>
                             <li className="mb-2">
-                                <a href="#" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Contact Us</a>
+                                <a href="/" className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Contact Us</a>
                             </li>
                         </ul>
                     </div>
                     <div className="px-4 mt-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
                         <h5 className="text-xl font-bold mb-6 sm:text-center xl:text-left">Stay connected</h5>
                         <div className="flex sm:justify-center xl:justify-start">
-                            <a href="" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-blue-600 hover:border-blue-600">
+                            <a href="/" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-blue-600 hover:border-blue-600">
                                 <i className="fab fa-facebook"></i>
                             </a>
-                            <a href="" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-blue-400 hover:border-blue-400">
+                            <a href="/" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-blue-400 hover:border-blue-400">
                                 <i className="fab fa-twitter"></i>
                             </a>
-                            <a href="" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-600">
+                            <a href="/" className="w-8 h-8  border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-600">
                                 <i className="fab fa-google-plus-g"></i>
                             </a>
                         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const products = [
         name: 'Earthen Bottle',
         href: '#',
         price: '$48',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+        imageSrc: 'https://img.freepik.com/free-vector/online-shop-shopping-icons-with-flat-design_23-2147653575.jpg?auto=format&h=200',
         imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
     {
@@ -14,7 +14,7 @@ const products = [
         name: 'Nomad Tumbler',
         href: '#',
         price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+        imageSrc: 'https://img.freepik.com/premium-vector/shopping-online-store-via-mobile-phone-bags-shopping-cart-gift-boxes-standing-upon-podium_181870-168.jpg?auto=format&h=200',
         imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
     {
@@ -22,7 +22,7 @@ const products = [
         name: 'Focus Paper Refill',
         href: '#',
         price: '$89',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        imageSrc: 'https://img.freepik.com/premium-vector/phone-mobile-store-line-organic-eco-products_269504-445.jpg?auto=format&h=200',
         imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
     },
     {
@@ -30,7 +30,7 @@ const products = [
         name: 'Machined Mechanical Pencil',
         href: '#',
         price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        imageSrc: 'https://img.freepik.com/premium-vector/3d-illustration-online-store-app-desktop-with-keyboard-basket-full-products-carry-bag-medical-mask-sanitizer-bottle-gift-boxes-blue-background_1302-22610.jpg?auto=format&h=200',
         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
     // More products...
